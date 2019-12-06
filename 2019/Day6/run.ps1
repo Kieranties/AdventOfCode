70be04d49a5dcd29fa4ef7b3cd31aef6b8ab0370
Kieran Marron <kieran.marron@sitecore.net>
2019-12-06 17:01:06 +0000

    2019 - Day 6 - Part 2

--- 2019/Day6/run.ps1
+++ 2019/Day6/run.ps1
@@ -4,19 +4,30 @@ param(
 
 $checksums = Get-Content $InputPath
 
+class GraphIndex {
+    [int]$Index
+    [string]$Name
+}
+
 class Orbit {
     [System.Collections.Generic.List[Orbit]]$Children
+    [string] $Name
+    [string] $Parent
     [int] $DirectOrbits
     [int] $TotalOrbits
+    [int] $Depth
     hidden [bool] $_counted
 
-    Orbit() {
+    Orbit([string]$Name) {
+        $this.Name = $Name
+        $this.Parent = [string]::Empty
         $this.Children = [System.Collections.Generic.List[Orbit]]::new()
         $this._counted = $false
     }
 
     [int] CountOrbits([int] $depth) {
         if(!$this._counted) {
+            $this.Depth = $depth
             $this.DirectOrbits = $this.Children.Count
             $this.TotalOrbits = $depth
             $depth++
@@ -41,6 +52,7 @@ function Map {
 
     begin {
         $map = [System.Collections.Generic.Dictionary[string, [Orbit]]]::new()
+        [string]$parent = [string]::Empty
     }
 
     process {
@@ -52,18 +64,21 @@ function Map {
             if($_ = $map[$left]) {
                 $body = $_
             } else {
-                $body = [Orbit]::new()
+                $body = [Orbit]::new($left)
                 $map[$left] = $body
             }
 
             if($_ = $map[$right]) {
                 $sattelite = $_
             } else {
-                $sattelite = [Orbit]::new()
+                $sattelite = [Orbit]::new($right)
                 $map[$right] = $sattelite
             }
 
+            $parent = $left
+            $sattelite.Parent = $parent
             $body.Children.Add($sattelite)
+
         }
     }
 
@@ -72,11 +87,49 @@ function Map {
     }
 }
 
+function Tour {
+    param (
+        [Orbit]$Orbit,
+        [System.Collections.Generic.List[GraphIndex]]$List,
+        [int]$Counter
+    )
+
+    $self = [GraphIndex]@{
+        Name = $orbit.Name
+        Index = $Counter
+    }
+
+    $list.Add($self)
+
+    foreach($child in $orbit.Children) {
+        $Counter++
+        Tour $child $List $counter
+        $list.Add($self)
+    }
+
+}
+
 $map = $checksums | Map
 $root = $map['COM']
 
 $answer1 = $root.CountOrbits(0)
 Write-Host "Answer1 : $answer1"
 
-$answer2 = ''
+$result = [System.Collections.Generic.List[GraphIndex]]::new()
+Tour $root $result 1
+$youParent = $map[($map['YOU'].Parent)]
+$sanParent = $map[($map['SAN'].Parent)]
+
+$youIndex = $result.FindIndex({$args[0].Name -eq $youParent.Name})
+$sanIndex = $result.FindIndex({$args[0].Name -eq $sanParent.Name})
+
+$slice = $result[($youIndex+1)..($sanIndex)]
+$lca = ($slice | Sort-Object -Property index)[0].Name
+$lcaOrbit = $map[$lca]
+
+
+$answer2 = ($youParent.Depth - $lcaOrbit.Depth) + ($sanParent.Depth - $lcaOrbit.Depth)
+
+
+
 Write-Host "Answer2 : $answer2"
